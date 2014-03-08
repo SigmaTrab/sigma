96404e90947e81cf069066bb535fe67a7cc41c31
LeandroPS <leandro.pires.souza@gmail.com>
2014-03-08 00:05:35 -0300

    Adicionando método matricular à classe 'Curso'

--- Curso.py
+++ Curso.py
@@ -6,6 +6,10 @@ class Curso:
 		self.turno = turno
 		self.codigo = Curso.cadastrar(self)
 		self.disciplinas = {}
+		self.alunos = {}
 
 	def incluir_disciplina(self, disciplina):
 		self.disciplinas[disciplina.codigo] = disciplina
+		
+	def matricular(self, aluno):
+    self.alunos = aluno
