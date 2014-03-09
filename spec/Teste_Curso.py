96de96bc9fde7d053f2ff30de9fd462ee71bbdef
LeandroPS <leandro.pires.souza@gmail.com>
2014-03-08 22:11:41 -0300

    Correção dos testes

--- spec/Teste_Curso.py
+++ spec/Teste_Curso.py
@@ -13,17 +13,17 @@ class Teste_Curso(unittest.TestCase):
 		um_curso.turno |should| equal_to('N')
 		um_curso.codigo |should| equal_to(222)	
 	
-	def teste_definir_nome(self, nome):
+	def teste_definir_nome(self):
 		um_curso = Curso('Soldador','N', 222)
 		um_curso.definir_nome('Tecnico em Informatica')
 		um_curso.nome |should| equal_to('Tecnico em Informatica')
 		
-	def teste_definir_turno(self, turno):
+	def teste_definir_turno(self):
 		um_curso = Curso('Soldador','N', 222)
 		um_curso.definir_turno('M')
 		um_curso.turno |should| equal_to('M')
 	
-	def teste_definir_codigo(self, codigo):
+	def teste_definir_codigo(self):
 		um_curso = Curso('Soldador','N', 222)
 		um_curso.definir_codigo(333)
 		um_curso.codigo |should| equal_to(333)
