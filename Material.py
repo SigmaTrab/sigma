42f008f71c9494d8e22eb42c8ec73b5f1bc34707
LeandroPS <carvalho.andrecampos@gmail.com>
2014-03-09 23:16:35 -0300

    criação da classe Material

--- Material.py
+++ Material.py
@@ -1,12 +1,14 @@
 class Material:
 	materiais = {}
 
-	def __init__(self, nome, disciplina):
+	def __init__(self, nome, conteudo):
 		self.nome = nome
-		self.disciplina = disciplina
+		self.conteudo = conteudo
+		self.bolsista_responsavel = bolsista
 
-	def Formatar_material(self, material, disciplina, bolsista):
-		self.material = material
-		self.disciplina = disciplina
-		self.bolsista = bolsista
+	def alterar_nome(self, nome):
+		self.nome = nome
+	
+	def alterar_conteudo(self, conteudo):
+		self.conteudo = conteudo
 
